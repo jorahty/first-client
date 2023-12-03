b9de42bcdc302a50d6b4f8c663d549beab4a5d15
James Tennant <jtennant@ucsc.edu>
2023-12-03 13:32:32 -0800

    talk to matchmaker

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -1,27 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
 import 'package:toolmax/pages/game/page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late io.Socket _socket;
+
+  bool isConnected = false;
+
+  connect() {
+    _socket = io.io(
+      'http://68.183.248.228:4000/',
+      io.OptionBuilder().setTransports(['websocket']).build(),
+    );
+
+    setState(() => isConnected = true);
+
+    _socket.on('invite', handleInvite);
+  }
+
+  disconnect() {
+    _socket.disconnect();
+
+    setState(() => isConnected = false);
+  }
+
+  void handleInvite(dynamic invite) {
+    disconnect();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (BuildContext context) => GamePage(
+          socketUri: 'http://${invite[0]}:${invite[1]}',
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff3a4260),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) => const GamePage(
-                  socketUri: 'http://138.197.216.1:7202',
-                ),
-              ),
-            );
-          },
-          child: const Text('Play'),
-        ),
+      backgroundColor: Colors.grey,
+      body: Column(
+        children: [
+          Text(isConnected ? 'Searching...' : ''),
+          ElevatedButton(
+            onPressed: isConnected ? disconnect : connect,
+            child: Text(isConnected ? 'Cancel' : 'Play'),
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _socket.disconnect();
+    super.dispose();
   }
 }
